76bdaf87879c0dcf6af6dec99a094dde71c1c8e3
Gurthy23 <106256487+Gurthy23@users.noreply.github.com>
2022-06-01 09:26:07 -0400

    Base code for bubble sort (one error)
    Code for the bubble sort but with one error

---
+++ BubbleSort/MyWorld.java
@@ -0,0 +1,63 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+    int[] array;
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        int x = 8;
+        for(int i=0;i<x;i++)
+        {
+            array[i] = GetRandomNumber(1,99);
+        }
+        prettyPrint();
+        BubbleSort();
+    }
+    public int GetRandomNumber(int start,int end)
+    {
+       int normal = Greenfoot.getRandomNumber(end-start+1);
+       return normal+start;
+    }
+    public void MyWorld()
+    {
+        array = new int[0];
+    }
+    void prettyPrint()
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.print(array[i] + " ");
+        }
+        
+        System.out.println();
+    }
+    public void BubbleSort()
+    {
+        for(int l = 0; l < array.length; l++)
+        {
+            for(int i = 0; i < array.length-1; i++)
+            {
+                if(array[i] > array[i+1])
+                {
+                    int x = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = x;
+                }
+            }
+            prettyPrint();
+        }
+    }
+}
